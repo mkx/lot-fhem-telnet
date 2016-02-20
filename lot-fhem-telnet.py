52285bd8fe78816757898b0aa1d2b1711771fbcf
Michael Krax <mk@krax.net>
2016-02-20 14:01:16 +0100

    works-for-me(TM) state

---
+++ lot-fhem-telnet.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import socket, threading
+import json
+import re
+
+FROM_PORT = 50140
+
+ 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('', 7172))
+s.listen(1)
+ 
+lock = threading.Lock()
+ 
+welcome_message = 'Welcome to LanguageOfThings FHEM interface\n'
+ 
+class daemon(threading.Thread):
+ 
+    def __init__(self, (socket,address)):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.address = address
+ 
+    def run(self):
+ 
+        sock = socket.socket(socket.AF_INET, # Internet
+                             socket.SOCK_DGRAM) # UDP
+
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock.bind(('', FROM_PORT))
+
+        # display welcome message
+        self.socket.send(welcome_message)
+
+        while True:
+            # read from LoT
+            data, addr = sock.recvfrom(1024)
+            j = json.loads(data)
+            id = j["id"]
+            data = j["data"]
+            # send to telnet
+
+            m = re.match(r"([\D]*)([.\d]*)", data[0])
+            data = ": ".join(m.groups())
+            
+            self.socket.send("TEMP %s %s\n" % (id, data));
+ 
+        # close connection
+        self.socket.close()
+ 
+while True:
+    daemon(s.accept()).start()
+
+    
